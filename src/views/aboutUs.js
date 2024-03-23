dbeca401683417182ec56ccbfe60fbb93303e713
edijavier99 <126507385+edijavier99@users.noreply.github.com>
2024-03-23 00:32:54 +0000

    contact 100 % and about 20% started

--- src/views/aboutUs.js
+++ src/views/aboutUs.js
@@ -1,9 +1,41 @@
 import React from "react";
+import "../styles/about.css"
 
 export const AboutUs = () =>{
     return(
-        <section>
-            <h1>About Us</h1>
+        <section className="container-fluid">
+            <header className="row">
+                <div className="col-12 about-back-img d-flex flex-column align-items-center justify-content-center ">
+                    <h3 className="about-out-title">COMPANY PROFILE</h3>
+                    <h1 className="about-out-title">ABOUT US</h1>
+                </div>
+            </header>
+            <div className="row col-md-10">
+            <div className="col-md-5 abt-intro-img-board d-flex flex-column justify-content-center align-items-end">
+                    <img className="abt-up-img mb-4 col-8" src="https://cdn.pixabay.com/photo/2015/07/17/22/42/startup-849804_1280.jpg" alt="" />
+                    <img className="abt-bottom-img col-8" src="https://cdn.pixabay.com/photo/2015/07/17/22/42/startup-849804_1280.jpg" alt=""/>
+                </div>
+                <div className="col-md-7 p-5">
+                    <h2>Let tomorrow begin today:</h2>
+                    <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+                    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+                     </p>
+                    <h3>Our Mission</h3>
+                    <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum  </p>
+                    <h3>Our Vision</h3>
+                    <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum  </p>
+                    <h3>Our Strategy</h3>
+                    <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum  </p>
+                </div>
+            </div>
+            <div className="row">
+                <div>
+
+                </div>
+            </div>
+            <div className="row col-md-10">
+
+            </div>
         </section>
     )
 }
