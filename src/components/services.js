5346d5f84da1fa1838cbb7c057d693bcd696675f
edijavier99 <126507385+edijavier99@users.noreply.github.com>
2024-06-23 01:04:23 +0100

    services changed and also the timeline changed with mui components and fade in effect added

--- src/components/services.js
+++ src/components/services.js
@@ -1,77 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/services.css";
 
 export const Services = () => {
   return (
-    <section id="services" className="container-fluid">
+    <section id="services">
       <p className="category text-muted">SERVICES</p>
-      <h2>What Services We Provide</h2>
+      <h2>What Problems We Solve</h2>
       <p className="services-description text-muted">At our company, we specialize in delivering<strong> top-notch web development, 
         software solutions, regular updates, and comprehensive maintenance services.</strong>
          Our expert team ensures that your digital presence is always <strong> cutting-edge, 
          efficient, and secure. </strong> With our regular updates and proactive maintenance, 
          your operations will run smoothly and without interruption. Trust us to 
          enhance your business with innovative and reliable technology solutions.</p>
-      <div className="pill-container">
-        <div className="pill">
-          <h1>Web Development</h1>
-          <div className="icon-container">
-            {/* <i className="fab fa-chrome mb-4"></i> */}
-            <img alt="" src="https://media-public.canva.com/48ulE/MAFrTN48ulE/1/s.png" />
-          </div>
-          <p>
-            Our web development services provide cutting-edge solutions to create responsive and user-friendly websites. We ensure your online presence is impactful and engaging for your audience.
-          </p>
-          <div className=" services-btn-card">
-              <div className="services-card-inner">
+      
+      <div id="first-service-article" className="container-fluid ">
+          <div className="row col-10 mx-auto service-article p-4 ">
+              <div className="col-12 col-md-6 col-lg-6">
+                <header className="d-flex align-items-center">
+                  <i class="fa-solid fa-code"></i>
+                  <h6 className="mb-0 ms-3 fw-bold">Websites Development</h6>
+                </header>
+                <main>
+                    <h3 className="fw-bold my-5">
+                    Our web development services provide cutting-edge solutions to 
+                    create responsive and user-friendly websites. We ensure your
+                     online presence is impactful and engaging for your audience.
+                    </h3>
+                    <hr/>
+                    <p className="fw-bold text-muted">Main expecifications</p>
+                    <ul>
+                      <li> <i class="fa-solid fa-square-check me-3"></i> Website Desing</li>
+                      <li> <i class="fa-solid fa-square-check me-3" ></i> Bussines Branding</li>
+                      <li> <i class="fa-solid fa-square-check me-3"></i> Website Desing</li>
+                    </ul>
+                </main>
+                <button className="btn btn-primary my-3">Get In Touch</button>
+              </div>
+              <div className="col-12 col-md-6 col-lg-6 article-img-container">
+                  <img src="https://images.unsplash.com/photo-1547658719-da2b51169166?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
               </div>
           </div>
-          
-        </div>
-        <div className="pill">
-          <h1>Software Solutions</h1>
-          <div className="icon-container">
-            {/* <i className="fa-solid fa-gear"></i> */}
-            <img alt="" src="https://media-public.canva.com/LY9qk/MAFfOTLY9qk/1/tl.png" />
-          </div>
-          <p>
-            Our software solutions cater to your specific needs, providing efficient and effective tools to streamline your operations. From custom software development to off-the-shelf solutions, we have you covered.
-          </p>
-          <div className=" services-btn-card">
-              <div className="services-card-inner">
+      </div>
+
+      <div id="first-service-article" className="container-fluid mt-4">
+          <div className="row col-10 mx-auto service-article-two p-4 ">
+          <div className="col-12 col-md-6 col-lg-6 article-img-container">
+                  <img src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+              </div>
+              <div className="col-12 col-md-6 col-lg-6">
+                <header className="d-flex align-items-center">
+                  <i class="fa-solid fa-code"></i>
+                  <h6 className="mb-0 ms-3 fw-bold">Software Development</h6>
+                </header>
+                <main>
+                    <h3 className="fw-bold my-5">
+                    Our web development services provide cutting-edge solutions to 
+                    create responsive and user-friendly websites. We ensure your
+                     online presence is impactful and engaging for your audience.
+                    </h3>
+                    <hr/>
+                    <p className="fw-bold text-muted">Main expecifications</p>
+                    <ul>
+                      <li> <i class="fa-solid fa-square-check me-3"></i> Website Desing</li>
+                      <li> <i class="fa-solid fa-square-check me-3" ></i> Bussines Branding</li>
+                      <li> <i class="fa-solid fa-square-check me-3"></i> Website Desing</li>
+                    </ul>
+                </main>
+                <button className="btn btn-primary my-3">Get In Touch</button>
               </div>
           </div>
-          
-        </div>
-        <div className="pill">
-          <h1>Regular Updates</h1>
-          <div className="icon-container">
-            {/* <i className="fa-solid fa-circle-up mb-4"></i> */}
-            <img alt="" src="https://media-public.canva.com/fFYoA/MAFRYDfFYoA/1/tl.png" />
-          </div>
-          <p>
-            Our regular updates ensure that your digital assets are always up-to-date with the latest features and security patches. Stay ahead of the curve with our proactive approach to maintenance and updates.
-          </p>
-          <div className=" services-btn-card">
-              <div className="services-card-inner">
-              </div>
-          </div>
-          
-        </div>
-        <div className="pill">
-            <h1>Maintenance</h1>
-            <div className="icon-container">
-                {/* <i class="fa-solid fa-toolbox"></i> */}
-                <img alt="" src="https://media-public.canva.com/d6qv0/MAFSQ1d6qv0/1/tl.png" />
-            </div>
-            <p>
-              Our comprehensive maintenance services keep your digital infrastructure running smoothly and efficiently. From server maintenance to bug fixes, we handle it all so you can focus on your core business.
-            </p>
-            <div className=" services-btn-card">
-                <div className="services-card-inner">
-                </div>
-            </div>
-        </div>
       </div>
     </section>
   );
--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,36 +1,131 @@
 import React from "react";
 import "../styles/timeline.css";
-import { TimelineCard } from "./timelinecard";
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import StepContent from '@mui/material/StepContent';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+
+const steps = [
+  {
+    label: 'PROJECT KICKOFF',
+    title: 'Understanding Your Vision',
+    description: `In this crucial first step, we hold a detailed meeting with
+    you to understand your project requirements, business goals, target 
+    audience, and any specific functionalities you need. We also discuss 
+    the project timeline and budget to ensure that we have a clear 
+    understanding of your vision. Our goal is to ensure we capture every 
+    detail, laying a strong foundation for a successful project.`,
+    optional: <Typography variant="caption">Step 1</Typography>,
+  },
+  {
+    label: 'DESIGN PHASE',
+    title: 'Bringing Ideas to Life',
+    description: `Our design team creates wireframes that outline the basic structure
+    and layout of your website or application. Once approved, we 
+    develop high-fidelity mockups that incorporate your branding, 
+    color schemes, and visual elements. This phase is all about 
+    bringing your ideas to life, ensuring that the visual representation 
+    aligns perfectly with your brand identity and resonates with your audience.`,
+    optional: <Typography variant="caption">Step 2</Typography>,
+  },
+  {
+    label: 'DEVELOPMENT PHASE',
+    title: 'Building the Future',
+    description: `Our skilled developers take the approved designs and transform 
+    them into a fully functional website or application. We use the latest 
+    technologies to ensure high performance, scalability, and security. 
+    This phase is where the magic happens, turning static designs into 
+    interactive, dynamic platforms that offer a seamless user experience.`,
+    optional: <Typography variant="caption">Step 3</Typography>,
+  },
+  {
+    label: 'LAUNCH & MAINTENANCE',
+    title: 'Going Live and Beyond',
+    description: `The final step involves deploying your website or 
+    application to a live server and making it accessible to your users.
+    We offer ongoing support and maintenance services to keep your site
+    or app updated and running efficiently. This ensures that your 
+    digital presence remains robust, secure, and up-to-date, allowing 
+    you to focus on your core business.`,
+    optional: <Typography variant="caption">Last step</Typography>,
+  },
+];
 
 export const Timeline = () => {
-    return (
-        <section id="timeline" className="container-fluid">
-            <p className="category text-muted">PROJECTS TIMELINE</p>
-            <h2>Workflow Proccess</h2>
-           <TimelineCard
-                title ="Project Kickoff"
-                subtitle="Initial Consultation"
-                description ="In this crucial first step, we hold a detailed meeting with you to understand your project requirements, business goals, target audience, and any specific functionalities you need. We also discuss the project timeline and budget to ensure that we have a clear understanding of your vision."
-                stickColor = "tomato" // Tomato
-           />
-           <TimelineCard
-                title ="Design Phase"
-                subtitle="Wireframes & Mockups"
-                description ="Our design team creates wireframes that outline the basic structure and layout of your website or application. Once approved, we develop high-fidelity mockups that incorporate your branding, color schemes, and visual elements."
-                stickColor = "steelblue" // Steel Blue
-           />
-           <TimelineCard
-                title ="Development Phase"
-                subtitle="Coding & Implementation"
-                description ="Our skilled developers take the approved designs and transform them into a fully functional website or application. We use the latest technologies to ensure high performance, scalability, and security."
-                stickColor = "green" // Lime Green
-           />
-           <TimelineCard
-                title ="Launch & Maintenance"
-                subtitle="Deployment & Support"
-                description ="The final step involves deploying your website or application to a live server and making it accessible to your users. We offer ongoing support and maintenance services to keep your site or app updated and running efficiently."
-                stickColor = "saddlebrown" // Saddle Brown
-           />
-        </section>
-    );
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <section id="timeline" className="container-fluid">
+      <p className="category text-muted">PROJECTS TIMELINE</p>
+      <h2>Workflow Process</h2>
+      <div className="col-8 mx-auto">
+        <Box>
+          <Stepper activeStep={activeStep} orientation="vertical">
+            {steps.map((step, index) => (
+              <Step key={step.label}>
+                <StepLabel
+                  optional={step.optional}
+                >
+                  {step.label}
+                </StepLabel>
+                <StepContent>
+                  <Typography variant="h6" fontWeight="bold">{step.title}</Typography>
+                  <Typography>{step.description}</Typography>
+                  <Box sx={{ mb: 2 }}>
+                    <div>
+                      <Button
+                        variant="contained"
+                        onClick={handleNext}
+                        sx={{ mt: 1, mr: 1 }}
+                      >
+                        {index === steps.length - 1 ? 'Finish' : 'Continue'}
+                      </Button>
+                      <Button
+                        disabled={index === 0}
+                        onClick={handleBack}
+                        sx={{ mt: 1, mr: 1 }}
+                      >
+                        Back
+                      </Button>
+                    </div>
+                  </Box>
+                </StepContent>
+              </Step>
+            ))}
+          </Stepper>
+          {activeStep === steps.length && (
+            <Paper square elevation={0} sx={{ p: 3, borderRadius: "20px", bgcolor: 'white', marginTop: "20px" }}>
+              <Typography variant="h6" gutterBottom>
+                Project Successfully Completed
+              </Typography>
+              <Typography>
+                Congratulations! Your project has been successfully launched and is now live. 
+                Your business is ready to engage with your audience. We are here to provide 
+                ongoing support and ensure everything runs smoothly.
+              </Typography>
+              <Button onClick={handleReset} sx={{ mt: 2 }}>
+                Reset
+              </Button>
+            </Paper>
+          )}
+        </Box>
+      </div>
+    </section>
+  );
 }
