d5473c9a8bfd881a9447cf74a399a368c6ec38b5
edijavier99 <edijavier10@gmail.com>
2025-09-10 14:51:24 +0100

    fixed: logo width fixed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -13,7 +13,7 @@ export const Navbar = (props) => {
 
   return (
     <nav className="navbar navbar-expand-lg">
-      <a className="navbar-brand" href="/"><img alt="logo" src={Logo}/></a>
+      <a className="navbar-brand" href="/"><img alt="logo" className="w-75" src={Logo}/></a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
